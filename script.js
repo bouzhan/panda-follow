f77d7780683c53b4758adc9fab2ab268ebe3a03b
bouzhan <bouzhan.saran@gmail.com>
2026-07-22 14:07:36 +0430

    json updated

--- script.js
+++ script.js
@@ -26,11 +26,51 @@ document.getElementById("vfv-open-post").onclick = () => {
   startTimer();
 };
 
+document.getElementById("vfv-req-sumbit").onclick = () => {
+  let link = document.getElementById("vfv-req-link-input").value;
+  if (!link.startsWith("https://t.me/")) {
+    document.getElementById("vfv-logs").textContent = "فرمت لینک باید با https://t.me شروع شود";
+    return;
+  }
+
+  SendLink(link);
+};
+
+async function SendLink(link) {
+  const url = "";
+
+  const data = {
+    link: link,
+  };
+
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const result = await response.json();
+    console.log("✅ پاسخ دریافت شد:", result);
+    return result;
+
+  } catch (error) {
+    console.error("❌ خطا در ارسال درخواست:", error);
+  }
+}
+
 document.addEventListener("visibilitychange", async () => {
   if (!document.hidden) {
-    if (time != 0){
-      document.getElementById("vfv-view-count").style.color = 'red';
-      document.getElementById("vfv-view-count").innerHTML = "زود تر از زمان برگشتی از اول باید پست رو ببینی";
+    if (time != 0) {
+      document.getElementById("vfv-view-count").style.color = "red";
+      document.getElementById("vfv-view-count").innerHTML =
+        "زود تر از زمان برگشتی از اول باید پست رو ببینی";
       document.getElementById("vfv-open-post").disabled = false;
       clearInterval(timer);
     }
@@ -38,9 +78,7 @@ document.addEventListener("visibilitychange", async () => {
 });
 
 function startTimer() {
-
   timer = setInterval(() => {
-    
     time--;
     document.getElementById("vfv-timer-counter").innerHTML = time;
 
@@ -50,7 +88,9 @@ function startTimer() {
       links_id++;
       view_count++;
 
-      document.getElementById("vfv-view-count").innerHTML = `تعداد بازی های شما: 0 از  ${view_count}`;
+      document.getElementById("vfv-view-count").innerHTML =
+        `تعداد بازدید های شما: ${view_count} از 100`;
+      document.getElementById("vfv-view-count").style.color = "black";
     }
   }, 1000);
 }
